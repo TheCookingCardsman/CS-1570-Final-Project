fd9ea6d7ed667190829b1353bbd047269924ca7b
Sreevaishnav "Vai" Deevi <31677444+TheCookingCardsman@users.noreply.github.com>
2017-12-03 23:55:12 -0600

    Partially added functionality for moving away from two whales, but still have many problems

--- penguin.cpp
+++ penguin.cpp
@@ -1,9 +1,14 @@
 #include "penguin.h"
+#include "fish.h"
+#include "killer_whale.h"
+#include "sea.h"
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
 #include <cmath>
 using namespace std;
+
+// BIG PROBLEM: HOW TO CODE FOR THE AMOUNT OF TIMES THE PENGUIN IS MOVING ACCURATELY BY STILL UPDATING EVERYTHING AFTER EACH TURN??
 
 Penguin::Penguin()
 {
@@ -21,7 +26,7 @@ void Penguin::move(Sea seaArr[], Fish fishArr[], Killer_Whale *whale1, Killer_Wh
   // First check to see if whale is near by. Same code for whether a whale exists or fish and whale exist
   if (sqrt(pow(xCord - whale1.getXCord(),2) + pow(yCord - whale1.getYCord(),2)) <= 8 && sqrt(pow(xCord - whale1.getXCord(),2) + pow(yCord - whale1.getYCord(),2)) <= 8) // Both whales within 8 units
   {
-
+    moveIfTwoWhales(seaArr[], whale1, whale2);
   } 
   else if (sqrt(pow(xCord - whale1.getXCord(),2) + pow(yCord - whale1.getYCord(),2)) <= 8) // Only whale1 is within 8 units
   {
@@ -51,10 +56,11 @@ void Penguin::move(Sea seaArr[], Fish fishArr[], Killer_Whale *whale1, Killer_Wh
     if (closeFish > 0) 
     {
       moveToFish(closeFish, seaArr[], fishArr[]); // Function to move the penguin closer to the closest fish. IN PROGRESS
-    }
+    }                                                                                                                                                                                   
   }
 }
 
+//-------------------------------------------------------------------------------------------------------
 // Description: Moves penguin a designated number of times towards the closest fish available
 // Precondition: 
 // Postcondition: 
@@ -62,28 +68,7 @@ void Penguin::move(Sea seaArr[], Fish fishArr[], Killer_Whale *whale1, Killer_Wh
 // NOTE: CURRENTLY NOT CODED TO HANDLE INTERFERING PENGUINS AND STOPPING PENGUIN MOVEMENT WHEN FISH IS EATEN
 void Penguin::moveToFish(int closeFish, Sea seaArr[], Fish fishArr[])
 {
-  // Sets the number of times the loop will go through based upon the health of the penguin
-  int loopNum;
-  if (health >= 81 && health <= 100)
-  {
-    loopNum = 5;
-  }
-  else if (health >= 61 && health <= 80)
-  {
-    loopNum = 4;
-  }
-  else if (health >= 41 && health <= 60)
-  {
-    loopNum = 3;
-  }
-  else if (health >= 21 && health <= 40)
-  {
-    loopNum = 2;
-  }
-  else if (health >= 1 && health <= 20)
-  {
-    loopNum = 1;
-  }
+  int loopNum = numMoves()
   
   for (int i = 0; i < loopNum; i++)
   {
@@ -144,7 +129,7 @@ void Penguin::moveToFish(int closeFish, Sea seaArr[], Fish fishArr[])
 // Precondition: 
 // Postcondition: 
 
-// NOTE: NOTE: CURRENTLY NOT CODED TO HANDLE INTERFERING PENGUINS 
+// NOTE: CURRENTLY NOT CODED TO HANDLE INTERFERING PENGUINS 
 void Penguin::moveIfOneWhale(Sea seaArr[], Killer_Whale *whale)
 {
   if (whale.getXCord() == xCord)
@@ -180,3 +165,67 @@ void Penguin::moveIfOneWhale(Sea seaArr[], Killer_Whale *whale)
 }
 
 //-------------------------------------------------------------------------------------------------------
+
+// Description: 
+// Precondition: 
+// Postcondition: 
+void moveIfTwoWhales(Sea seaArr[], Killer_Whale *whale1, Kille_Whale *whale2)
+{
+  if (whale1.getXCord() < xCord && whale2.getXCord() < xCord)
+  {
+    xCord += 1;
+  }
+  else if (whale1.getXCord() > xCord && whale2.getXCord() > xCord)
+  {
+    xCord -= 1;
+  }
+  else if (whale1.getXCord() == xCord && whale2.getXCord() == xCord)
+  {
+    xCord += 1; // Code code be going up or down based on prefrence
+  }
+  else if ((whale1.getXCord() < xCord && whale1.getYCord() < yCord && whale2.getXCord() > xCord && whale2.yCord() > yCord) || (whale2.getXCord() < xCord && whale2.getYCord() < yCord && whale1.getXCord() > xCord && whale1.yCord() > yCord))
+  {
+    xCord -= 1;
+    yCord += 1; 
+    // Coded to go diagonal to the right upward. Can be coded to go left as well
+  }
+  else if ((whale1.getXCord() > xCord && whale1.getYCord() < yCord && whale2.getXCord() < xCord && whale2.yCord() > yCord) || (whale2.getXCord() < xCord && whale2.getYCord() < yCord && whale1.getXCord() > xCord && whale1.yCord() > yCord))
+  {
+    xCord += 1;
+    yCord += 1;
+    // Coded to go diagional to the right downward. Can be coded to go left as well
+  }
+  else if ((whale1.getXCord() == yCord && whale2.getYCord() > yCord) || (whale2.getXCord() == yCord && whale1.getYCord() > yCord))
+  {
+    yCord -= 1;
+  }
+  else if ((whale1.getXCord() == yCord && whale2.getYCord < yCord) || (whale2.getXCord() == yCord && whale1.getYCord() < yCord))
+  {
+    yCord += 1;
+  }
+  else if ()
+}
+
+int Penguin::numMoves()
+{
+  if (health >= 81 && health <= 100)
+  {
+    return 5;
+  }
+  else if (health >= 61 && health <= 80)
+  {
+    return 4;
+  }
+  else if (health >= 41 && health <= 60)
+  {
+    return 3;
+  }
+  else if (health >= 21 && health <= 40)
+  {
+    return 2;
+  }
+  else if (health >= 1 && health <= 20)
+  {
+    return 1;
+  }
+}
