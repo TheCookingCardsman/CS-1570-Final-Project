76e40a33efbaa20f30b6a7f8c0137e2ba8dbcf6b
Sreevaishnav "Vai" Deevi <31677444+TheCookingCardsman@users.noreply.github.com>
2017-12-05 12:51:47 -0600

    Updated function prototypes as needed

--- penguin.h
+++ penguin.h
@@ -1,18 +1,23 @@
 #ifndef PENGUIN_H
 #define PENGUIN_H
 
-class Penguin 
+class Penguin
 {
-public: 
+public:
   Penguin();
-  void move(Sea seaArr[], Fish fishArr[]);
+  void move(Sea seaWorld, Fish fishArr[], Killer_Whale *whale1, Killer_Whale *whale2);
 private:
   int health;
   int xCord;
   int yCord;
   bool alive;
-  void moveToFish(closeFish, seaArr[], fishArr[]);
-  void moveIfOneWhale(Sea seaArr[], Killer_Whale *whale1);
-  void moveIfTwoWhales(Sea seaArr[], Killer_Whale *whale1, Kille_Whale *whale2); 
-  int numMoves(); 
+
+  void moveToFish(Sea *seaGrid, Fish fishArr[], int fishIndex, double distance);
+  void moveIfBlockedFish(Sea *seaGrid, Fish fishArr[], int fishIndex, double distance);
+  double fishDistance(Fish fish, int x, int y);
+  double whaleDist(Killer_Whale whale, int x, int y);
+  int numMoves();
+  void moveIfOneWhale(Sea *seaGrid, Killer_Whale *whale, double distWhale, Fish fishArr[]);
+  void moveIfTwoWhales(Sea *seaGrid, Killer_Whale *whale1, Kille_Whale *whale2, double distWhale1, double distWhale2, Fish fishArr[]);
+  void moveRandom(Sea *seaGrid);
 }
