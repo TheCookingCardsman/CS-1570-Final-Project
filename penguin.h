ef11333261a65f6de2c2906cfe415638dd9cf259
xAysee <31873853+xAysee@users.noreply.github.com>
2017-12-06 06:07:54 -0600

    Add files via upload
    ARE YOU HAPPY NOW

--- penguin.h
+++ penguin.h
@@ -1,18 +1,33 @@
 #ifndef PENGUIN_H
 #define PENGUIN_H
+class Sea;
+class Fish;
+class KillerWhale;
 
-// Do you want a two seperate coordinates or a struct? Your Choice.
-class Penguin 
+class Penguin
 {
-public: 
-  Penguin();
-  void move(Sea seaArr[], Fish fishArr[]);
-private:
-  int health;
-  int xCord;
-  int yCord;
-  bool alive;
-  void fishMove();
-  void whaleMove();
-  char closestAnimal();  
-}
+  public:
+    Penguin();
+    void movePeng(Sea &seaWorld, Fish fishArr[], KillerWhale &whale1, KillerWhale &whale2);
+    int getPositionX();
+    int getPositionY();
+    void setPositionX(int change);
+    void setPositionY(int change);
+  private:
+    int health;
+    int positionX;
+    int positionY;
+    bool alive;
+
+    void moveToFish(Sea &seaGrid, Fish fishArr[], int fishIndex, double distance);
+    void moveIfBlockedFish(Sea &seaGrid, Fish fishArr[], int fishIndex, double distance);
+    double fishDistance(Fish fish, int x, int y);
+    double whaleDist(KillerWhale whale, int x, int y);
+    int numMoves();
+    void moveIfOneWhale(Sea &seaGrid, KillerWhale &whale, double distWhale, Fish fishArr[]);
+    void moveIfTwoWhales(Sea &seaGrid, KillerWhale &whale1, KillerWhale &whale2, double distWhale1, double distWhale2, Fish fishArr[]);
+    void moveRandom(Sea &seaGrid);
+    int arrSize(Fish fishArr[]);
+};
+
+#endif
